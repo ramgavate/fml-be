8718b2f393e9cd5643c4166e54fe9ffc8a9c24f9
ramgavate <ram_gavate@bpointer.in>
2025-12-03 20:13:19 +0530

    Refactor code structure for improved readability and maintainability

--- config.js
+++ config.js
@@ -3,9 +3,14 @@
  */
 
 
-module.exports = {
-    //'database': 'mongodb://0.0.0.0:27017/crazycat-db'
-    //'database': 'mongodb://0.0.0.0:27017/fml-db'
+// module.exports = {
+//     //'database': 'mongodb://0.0.0.0:27017/crazycat-db'
+//     //'database': 'mongodb://0.0.0.0:27017/fml-db'
    
-    'database': 'mongodb+srv://FMLBPointer:BPointer123*@fmlapp.vaxky.mongodb.net/fml?w=majority'
+//     'database': 'mongodb+srv://FMLBPointer:BPointer123*@fmlapp.vaxky.mongodb.net/fml?w=majority'
+// };
+
+// Updated DB connection string on 06-06-2024 for local and testing db purposes
+module.exports = {
+  database: "mongodb+srv://bpointercommon_db_user:9KMZnkTmB4ybj9eN@fml.l8r3am7.mongodb.net/fml?retryWrites=true&w=majority"
 };
